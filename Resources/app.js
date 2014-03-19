1dba6efd30cde6adfb52fce1ca14d5241fb575c6
jordannick <jordann@onid.orst.edu>
2014-03-18 18:00:09 -0700

    Breakthroughs in shuttle tracking technology
    Updated table parser to refresh. Added vehicle coordinate parser.
    Separated the function in map.html into createMap and updateMap
    functions. Added corresponding events and event listeners.

--- Resources/app.js
+++ Resources/app.js
@@ -8,6 +8,7 @@ var tabGroup = Titanium.UI.createTabGroup();
 
 var url = "http://www.osushuttles.com/Services/JSONPRelay.svc/GetMapStopEstimates";
 var url2 = "http://www.osushuttles.com/Services/JSONPRelay.svc/GetRoutesForMapWithSchedule";
+var url3 = "http://www.osushuttles.com/Services/JSONPRelay.svc/GetMapVehiclePoints";
 
 
 //Tab 1/Window 1: contains table for stops
@@ -43,93 +44,99 @@ var tab3 = Titanium.UI.createTab({
 
 //===================================================================
 
+//Variables and function for filling the table with route and arrival info
 
 var table = Ti.UI.createTableView();
 var tableData = [];
 var shuttles, shuttle, i, j, row, nameLabel, idLabel, stopsText;
 
-var xhr = Ti.Network.createHTTPClient({
-    onload: function() {
-        
-    shuttles = JSON.parse(this.responseText);
+function dataRequest(){
+	var xhr = Ti.Network.createHTTPClient({
+    	onload: function() {
+     
+     		//Clear the table at start of new request
+    		tableData = [];
+			table.setData(tableData);
+     
+    		shuttles = JSON.parse(this.responseText);
     
-    for (i = 0; i < shuttles.length; i++) {
+    		for (i = 0; i < shuttles.length; i++) {
         
-        shuttle = shuttles[i]; //an individual route
-        
-        row = Ti.UI.createTableViewRow({
-            height:'60dp'
-        });
-        
-        nameLabel = Ti.UI.createLabel({ //name of route
-            text:shuttle.Description,
-            font:{
-                fontSize:'24dp',
-                fontWeight:'bold'
-            },
-        height:'auto',
-        left:'10dp',
-        top:'5dp',
-        color:'#000',
-        touchEnabled:true
-        });
-        
-        idLabel = Ti.UI.createLabel({ // ID of route
-            text:'Route ID: ' + shuttle.RouteID,
-            font:{
-                fontSize:'16dp'
-            },
-        height:'auto',
-        left:'15dp',
-        top:'30dp',
-        color:'#000',
-        touchEnabled:false
-        });
-        
-        row.add(nameLabel);
-        row.add(idLabel);
-        
-        tableData.push(row);
-        
-        //Look at locations along route
-        for (j = 0; j < shuttle.RouteStops.length; j++) {
-        
-            var innerRow = Ti.UI.createTableViewRow({
-                height:'16dp'
-            });
-            
-            var eta = shuttle.RouteStops[j].Estimates[0].SecondsToStop;
-            
-            if (eta == 0) {
-                eta = 'Arrived';
-                eta = eta.bold(); //untested
-            }
-            
-            else eta = eta + ' seconds';
-            
-            stopsText = Ti.UI.createLabel({ 
-                text:'Location: ' + shuttle.RouteStops[j].Description + ', ETA: ' + eta,
-                font:{
-                    fontSize:'14dp'
-                },
-            height:'auto',
-            left:'15dp',
-            color:'#000',
-            touchEnabled:false  
-            });
-            
-            innerRow.add(stopsText);
-            tableData.push(innerRow);
-        }
-    }
+	        	shuttle = shuttles[i]; //an individual route
+	        
+	        	row = Ti.UI.createTableViewRow({
+	            	height:'60dp'
+	        	});
+	        
+	        	nameLabel = Ti.UI.createLabel({ //name of route
+	            				text:shuttle.Description,
+	            				font:{
+	                				fontSize:'24dp',
+	                				fontWeight:'bold'
+	            				},
+	        					height:'auto',
+	        					left:'10dp',
+	        					top:'5dp',
+	        					color:'#000',
+	        					touchEnabled:true
+	        					});
+	        
+	        	idLabel = Ti.UI.createLabel({ // ID of route
+	            				text:'Route ID: ' + shuttle.RouteID,
+	            				font:{
+	                			fontSize:'16dp'
+	            				},
+	        					height:'auto',
+	        					left:'15dp',
+	        					top:'30dp',
+	        					color:'#000',
+	        					touchEnabled:false
+	        					});
+	        
+	        	row.add(nameLabel);
+	        	row.add(idLabel);
+	        
+	        	tableData.push(row);
+	        
+	        	//Look at locations along route
+	        	for (j = 0; j < shuttle.RouteStops.length; j++) {
+	        
+	            	var innerRow = Ti.UI.createTableViewRow({
+	                	height:'16dp'
+	            	});
+	            
+	            	var eta = shuttle.RouteStops[j].Estimates[0].SecondsToStop;
+	            
+	            	if (eta == 0) {
+	               		eta = '>>>Arrived<<<';
+	            	}
+	            	else eta = eta + ' seconds';
+	            
+	            	stopsText = Ti.UI.createLabel({ 
+	                				text:'Location: ' + shuttle.RouteStops[j].Description + ', ETA: ' + eta,
+	                				font:{
+	                    				fontSize:'14dp'
+	                				},
+	            					height:'auto',
+	            					left:'15dp',
+	            					color:'#000',
+	            					touchEnabled:false  
+	            					});
+	            
+	            	innerRow.add(stopsText);
+	            	tableData.push(innerRow);
+	        	}
+    		}
  
-    table.setData(tableData);
+    		table.setData(tableData);
+    
     },
+    
     onerror: function(e) {
-    Ti.API.debug("STATUS: " + this.status);
-    Ti.API.debug("TEXT:   " + this.responseText);
-    Ti.API.debug("ERROR:  " + e.error);
-    alert('There was an error retrieving the remote data. Try again.');
+    	Ti.API.debug("STATUS: " + this.status);
+    	Ti.API.debug("TEXT:   " + this.responseText);
+    	Ti.API.debug("ERROR:  " + e.error);
+    	alert('There was an error retrieving the remote data. Try again.');
     },
     timeout:5000
 });
@@ -137,10 +144,20 @@ var xhr = Ti.Network.createHTTPClient({
 xhr.open("GET", url);
 xhr.send();
 
+}
+
 win1.add(table);
 
-//===================================================================
+//initial request to fill table
+dataRequest();
+ 
+//repeat every 5 seconds
+setInterval(function() {
+	dataRequest();
+}, 5000);
 
+
+//===================================================================
 
 //Create webview of map.html
 var localWebview = Titanium.UI.createWebView({
@@ -176,7 +193,8 @@ Titanium.Geolocation.getCurrentPosition(function(e)
 				{
 					labelgps.text = 'error code:' + JSON.stringify(e.code);
 					//Ti.API.info("Code translation: "+translateErrorCode(e.code));
-					alert('error ' + JSON.stringify(e.error));
+					//alert('error ' + JSON.stringify(e.error));
+					//alert('Could not find mobile GPS unit');
 					return;
 				}
 				var longitude = e.coords.longitude;
@@ -193,29 +211,29 @@ Titanium.Geolocation.getCurrentPosition(function(e)
 				Titanium.API.info('geo - current location: ' + new Date(timestamp) + ' long ' + longitude + ' lat ' + latitude + ' accuracy ' + accuracy);
 			});
 			
+//Place in label	
 win3.add(labelgps);
 
 
 //===================================================================
 
-
-var x, y;
+//Variables and function for stop positions data
 var routes, route;
 var stops = new Array(7); //Hold list of stops
 for (var i=0;i<7;i++){
 	stops[i]=new Array(3); //Hold stop info: label, latitude, longitude
 }
 
-//Get list of stop coordinates, place in stops array
 var xhr2 = Ti.Network.createHTTPClient({
 	onload: function() {
 		
-		//Get all routes
+		//Get all route info
 		routes = JSON.parse(this.responseText);
 		
 		//Single route
 		route = routes[0];
 		
+		var x;
 		for (x=0;x<route.Landmarks.length;x++){
 			stops[x][0]=route.Landmarks[x].Label;
 			stops[x][1]=route.Landmarks[x].Latitude;
@@ -223,19 +241,67 @@ var xhr2 = Ti.Network.createHTTPClient({
 		}
 		
 	}
+	
+	//Should include onerror here
+	
 });
 
 xhr2.open("GET", url2);
 xhr2.send();
 
+
+//===================================================================
+
+//Variables and function for shuttle coordinates data
+
+var shuttleloc, shuttlelocs;
+var shuttlecoords = new Array(3); //Hold all shuttle data
+for (var i=0;i<3;i++){
+	shuttlecoords[i]=new Array(2); //Hold latitude, longitude
+}
+
+function ShuttleLocRequest(){
+	var xhr3 = Ti.Network.createHTTPClient({
+		onload: function() {
+		
+			//Get all info
+			shuttlelocs = JSON.parse(this.responseText);
+		
+			for (var x=0;x<shuttlelocs.length;x++){
+				shuttleloc = shuttlelocs[x];
+				shuttlecoords[x][0] = shuttleloc.Latitude;
+				shuttlecoords[x][1] = shuttleloc.Longitude;
+			}
+	}
+	
+	//Should include onerror here
+});
+
+xhr3.open("GET", url3);
+xhr3.send();
+
+}
+
+
 //===================================================================
 
 var dummyUser = ["dummy User GPS data", 44.5657, -123.2789];
 
-//Event listener to wait for call to send stops array and user GPS
+//Event listener to start when webview loads
 localWebview.addEventListener('load',function(){
-	Ti.App.fireEvent("web:data", {data: [stops, dummyUser]});
+	
+	//Start the create map event
+	Ti.App.fireEvent("startmap", {data: [stops, dummyUser]});
+
+	//Request the shuttle data, and start the update event, repeats every 5 seconds
+	setInterval(function() {
+		ShuttleLocRequest();
+		Ti.App.fireEvent("updatemap", {data: shuttlecoords});
+	}, 5000);
+	
 });
+
+//===================================================================
 
 
 //Start the tabs
@@ -243,3 +309,4 @@ tabGroup.addTab(tab1);
 tabGroup.addTab(tab2);
 tabGroup.addTab(tab3);
 tabGroup.open();
+
