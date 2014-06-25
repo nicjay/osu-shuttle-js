f8604a7bd838c42ba6d940b4a9b26ad2c89c1cdf
sellers04 <sellers.kevin@gmail.com>
2014-06-25 15:46:31 -0700

    Removed unneeded images

--- Resources/webview.js
+++ Resources/webview.js
@@ -102,7 +102,7 @@
         		//UserMarkerSymbol.setColor(new dojo.Color("#00FF00"));
         		//UserMarkerSymbol.setOutline(null);
         		
-        		var StopMarkerSymbol = new esri.symbol.PictureMarkerSymbol('GeneralUI/stopSign3.png', 45, 45);
+        		var StopMarkerSymbol = new esri.symbol.PictureMarkerSymbol('GeneralUI/stopSign.png', 20, 20);
         		//Cusom picture example for marker
         		//symbol = new PictureMarkerSymbol("images/bluedot.png", 40, 40);
         			
@@ -499,7 +499,7 @@
 				map.graphics.remove(selectStop);
 				
 
-      			var selectStopSymbol = new PictureMarkerSymbol('GeneralUI/stopSignSelected2.png', 45, 45);
+      			var selectStopSymbol = new PictureMarkerSymbol('GeneralUI/stopSignSelected.png', 45, 45);
 
 
       			selectStop = new Graphic(centerPoint, selectStopSymbol);
